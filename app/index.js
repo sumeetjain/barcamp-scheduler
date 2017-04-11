aefd6c946fe063192b386ace52d86c20e784309b
Jamie Locatis <jlocatis@gmail.com>
2017-04-11 10:38:23 -0500

    Added row of boxes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Barcamp = require('./components/row.js')
 
 const box = {
   width: "300px",
   height: "300px",
   backgroundColor: "#eee",
-  fontFamily: "Helvetica"
+  fontFamily: "Helvetica",
+  marginRight: "10px"
 };
 
 const title = {
@@ -28,4 +28,75 @@ const clear = {
   clear: "both"
 };
 
-ReactDOM.render(<Barcamp />, document.getElementById('app'));
+const row = {
+  display: "flex"
+}
+
+var Name = React.createClass({
+  render: function() {
+    return(
+      <div style={name}>{this.props.name}</div>
+    );
+  }
+});
+
+var Title = React.createClass({
+  render: function() {
+    return(
+      <div style={title}>{this.props.title}</div>
+    );
+  }
+});
+
+var CategoryImage = React.createClass({
+  render: function() {
+    return(
+      <div style={categoryImage}><img src={this.props.image} width="40" /></div>
+    );
+  }
+});
+
+var ClearFloats = React.createClass({
+  render: function() {
+    return(
+      <div style={clear}></div>
+    );
+  }
+});
+
+var Description = React.createClass({
+  render: function() {
+    return(
+      <div>{this.props.description}</div>
+    );
+  }
+});
+
+var Barcamp = React.createClass({
+  render: function() {
+    return (
+      <div style={box}>
+        <Name name="Jamie Locatis" />
+        <CategoryImage image="./app/images/track_creative_2016.png" />
+        <ClearFloats />
+        <Title title="This is a title of something" />
+        <Description description="Celiac kitsch poutine, tacos stumptown cardigan jianbing pug pickled kinfolk try-hard put a bird on it air plant activated charcoal. Swag kinfolk marfa cornhole, ennui try-hard narwhal blue bottle forage gastropub schlitz keytar. Bicycle rights schlitz farm-to-table woke biodiesel, shoreditch scenester four dollar toast fashion axe heirloom godard tbh DIY skateboard. Offal cliche hashtag franzen asymmetrical art party." />
+      </div>
+    );
+  }
+});
+
+var RowByTime = React.createClass({
+  render: function() {
+    return(
+    <div style={row}>
+      <Barcamp />
+      <Barcamp />
+      <Barcamp />
+      <Barcamp />
+    </div>
+    )
+  }
+});
+
+ReactDOM.render(<RowByTime />, document.getElementById('app'));
