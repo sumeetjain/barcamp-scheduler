f0bb7b6352142f81401c41b09b348d19b8156711
Ben Nourse <bnourse@gmail.com>
2017-04-11 16:23:55 -0500

    seeder fixed

--- bc-back/db/seeds.rb
+++ bc-back/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Cards.timeslots.keys.each do |slot|
-  Cards.categories.keys.each do |cat|
-    Card.create(timeslot: slot, category: cat, status: "SIGNUP")
+Card.timeslots.keys.each do |slot|
+  Card.categories.keys.each do |cat|
+    Card.create(timeslot: slot, category: cat, state: "SIGNUP")
   end
 end
