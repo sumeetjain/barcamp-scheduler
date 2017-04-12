088c80d752916092f63f257700883def60a75aae
Jamie Locatis <jlocatis@gmail.com>
2017-04-12 09:26:34 -0500

    Time is being served through Rails, page display is a loop

--- bc-back/app/controllers/card_controller.rb
+++ bc-back/app/controllers/card_controller.rb
@@ -1,7 +1,9 @@
 class CardController < ApplicationController
   def jsontest
+    time_test = ['9:00am', '9:30am', '10:00am', '10:30am', '11:00am', '11:30am', '12:00pm', '1:00pm', '1:30pm', '2:00pm', '2:30pm', '3:00pm', '3:30pm', '4:00pm']
+
     respond_to do |format|
-      format.json { render(json: "asdfghjkl") }
+      format.json { render(json: time_test) }
     end
   end
 end
--- bc-front/app/components/app.js
+++ bc-front/app/components/app.js
@@ -13,9 +13,8 @@ var App = React.createClass({
     xhr.open('GET', 'http://localhost:3000/ajaxtest');
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.onload = function() {
-      response = xhr.responseText;
+      response = JSON.parse(xhr.responseText);
       _this.setState({time:response});
-      debugger;
     }
     xhr.send();
   },
@@ -23,22 +22,12 @@ var App = React.createClass({
     this.serverRequest.abort();
   },
   render: function() {
+  var allRows = this.state.time.map((time) =>
+    <RowByTime time={time} />
+  );
     return(
       <div>
-        <RowByTime time={this.state.time} />
-        <RowByTime time="9:30am" />
-        <RowByTime time="10:00am" />
-        <RowByTime time="10:30am" />
-        <RowByTime time="11:00am" />
-        <RowByTime time="11:30am" />
-        <RowByTime time="12:00pm" />
-        <RowByTime time="1:00pm" />
-        <RowByTime time="1:30pm" />
-        <RowByTime time="2:00pm" />
-        <RowByTime time="2:30pm" />
-        <RowByTime time="3:00pm" />
-        <RowByTime time="3:30pm" />
-        <RowByTime time="4:00pm" />
+        {allRows}
       </div>
     )
   }
