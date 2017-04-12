d1de55f794f84c692cee5f4e879762050d9c7136
Jamie Locatis <jlocatis@gmail.com>
2017-04-12 08:46:26 -0500

    Trying out Axios

--- bc-front/app/components/app.js
+++ bc-front/app/components/app.js
@@ -2,6 +2,24 @@ var React = require('react');
 var RowByTime = require('./row.js');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      time: ''
+    }
+  },
+  componentDidMount: function() {
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:3000/ajaxtest');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onload = function() {
+      response = xhr.responseText
+      debugger;
+    }
+    xhr.send();
+  },
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
   render: function() {
     return(
       <div>
