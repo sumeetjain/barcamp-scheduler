e4148f84f8db62463c0fda5e98dc0064926afaaa
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-04-14 10:05:00 -0500

    enum to hash

--- bc-back/app/models/card.rb
+++ bc-back/app/models/card.rb
@@ -1,9 +1,31 @@
 class Card < ApplicationRecord
-  enum timeslot: ["9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", 
-    "11:30 AM", "12:00 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", 
-    "3:00 PM", "3:30 PM", "4:00 PM"]
-  enum category: ["Creative", "Entrepreneur", "Technology", "Kitchen Sink"]
-  enum state: ["SIGNUP", "PENDING", "SET"]
+  enum timeslot: {
+    "9:00 AM" => 0, 
+    "9:30 AM" => 1, 
+    "10:00 AM" => 2, 
+    "10:30 AM" => 3, 
+    "11:00 AM" => 4, 
+    "11:30 AM" => 5, 
+    "12:00 PM" => 6, 
+    "1:00 PM" => 7, 
+    "1:30 PM" => 8, 
+    "2:00 PM" => 9, 
+    "2:30 PM" => 10, 
+    "3:00 PM" => 11, 
+    "3:30 PM" => 12, 
+    "4:00 PM" => 13
+  }
+  enum category: {
+    "Creative" => 0,
+    "Entrepreneur" => 1, 
+    "Technology" => 2, 
+    "Kitchen Sink" => 3
+  }
+  enum state: {
+    "SIGNUP" => 0, 
+    "PENDING" => 1, 
+    "SET" => 2
+  }
 
   def self.json
     cards = Card.all.order(:timeslot).as_json
