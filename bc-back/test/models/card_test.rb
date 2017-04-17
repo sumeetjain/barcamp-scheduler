fe47e5b7edf517d0397c74ab4cb98471599bc8f7
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-04-17 10:24:12 -0500

    adjusted tests for cancel function

--- bc-back/test/models/card_test.rb
+++ bc-back/test/models/card_test.rb
@@ -1,41 +1,6 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-	test "changes state to pending" do
-		card = Card.find_by(id: 0)
-		card.statePending
-		assert_equal "PENDING", card.state, "state is pending"
-	end
-
-	test "changes state to sign-up" do
-		card = Card.find_by(id: 1)
-		card.stateSignUp
-		assert_equal "SIGNUP", card.state, "state is signup"
-	end
-
-	test "updates info" do
-		params = ActionController::Parameters.new({:id => 1, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
-		Card.setValues(params)
-		assert_equal "Han Solo", Card.where(:id => 1).pluck(:name)[0], "name updated"
-		assert_equal "The Millenium Falcon", Card.where(:id => 1).pluck(:title)[0], "title updated"
-		assert_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 1).pluck(:description)[0], "description updated"
-	end
-
-	test "doesn't update info if state is signup" do
-		params = ActionController::Parameters.new({:id => 0, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
-		Card.setValues(params)
-		assert_not_equal "Han Solo", Card.where(:id => 0).pluck(:name)[0], "name updated"
-		assert_not_equal "The Millenium Falcon", Card.where(:id => 0).pluck(:title)[0], "title updated"
-		assert_not_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 0).pluck(:description)[0], "description updated"
-	end
-
-	test "doesn't update info if state is set" do
-		params = ActionController::Parameters.new({:id => 2, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
-		Card.setValues(params)
-		assert_not_equal "Han Solo", Card.where(:id => 0).pluck(:name)[0], "name updated"
-		assert_not_equal "The Millenium Falcon", Card.where(:id => 0).pluck(:title)[0], "title updated"
-		assert_not_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 0).pluck(:description)[0], "description updated"
-	end
 
 	test "retrieves all data" do
 		# look at fixtures/cards.yml
@@ -57,6 +22,48 @@ class CardTest < ActiveSupport::TestCase
 		assert_includes jsonresult["9:00 AM"]["Creative"].keys, "description", "description in info"
 		assert_includes jsonresult["9:00 AM"]["Creative"].keys, "id", "id in info"
 		assert_includes jsonresult["9:00 AM"]["Creative"].keys, "state", "state in info"
+	end
+
+	test "changes state to pending" do
+		card = Card.find_by(id: 0)
+		card.statePending
+		assert_equal "PENDING", card.state, "state is pending"
+	end
+
+	test "updates info" do
+		params = ActionController::Parameters.new({:id => 1, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
+		Card.setValues(params)
+		assert_equal "Han Solo", Card.where(:id => 1).pluck(:name)[0], "name updated"
+		assert_equal "The Millenium Falcon", Card.where(:id => 1).pluck(:title)[0], "title updated"
+		assert_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 1).pluck(:description)[0], "description updated"
+	end
+
+	test "update info if state is signup" do
+		params = ActionController::Parameters.new({:id => 0, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
+		Card.setValues(params)
+		assert_equal "Han Solo", Card.where(:id => 0).pluck(:name)[0], "name updated"
+		assert_equal "The Millenium Falcon", Card.where(:id => 0).pluck(:title)[0], "title updated"
+		assert_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 0).pluck(:description)[0], "description updated"
+	end
+
+	test "doesn't update info if state is set" do
+		params = ActionController::Parameters.new({:id => 2, :name => "Han Solo", :title => "The Millenium Falcon", :description => "All about the the fastest ship in the galaxy."})
+		Card.setValues(params)
+		assert_not_equal "Han Solo", Card.where(:id => 0).pluck(:name)[0], "name updated"
+		assert_not_equal "The Millenium Falcon", Card.where(:id => 0).pluck(:title)[0], "title updated"
+		assert_not_equal "All about the the fastest ship in the galaxy.", Card.where(:id => 0).pluck(:description)[0], "description updated"
+	end
+
+	test "changes state to sign-up" do
+		params = ActionController::Parameters.new({:id => 1})
+		Card.cancel(params)
+		assert_equal "SIGNUP", Card.where(:id => 1).pluck(:state)[0], "state is signup"
+	end
+
+	test "doesn't change state to sign-up if set" do
+		params = ActionController::Parameters.new({:id => 2})
+		Card.cancel(params)
+		assert_not_equal "SIGNUP", Card.where(:id => 1).pluck(:state)[0], "state is not signup"
 	end
 
 end
